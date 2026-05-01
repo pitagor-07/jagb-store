550b34b355128555d69f7a382439a90f89b9a6de
pitagor-07 <lochardhenryclaude07@gmail.com>
2026-05-01 14:54:01 -0400

    Fix API session

--- server.js
+++ server.js
@@ -110,7 +110,7 @@ app.post('/api/commandes', async (req, res) => {
 });
 
 // --- API RAPPORT ADMIN ---
-app.get('/api/admin/rapport', async (req, res) => {
+undefined
     if (!req.session.loggedIn) return res.status(401).send("Non autorise");
     try {
         const commandes = await Commande.find().sort({ date: -1 });
